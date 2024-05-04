cc2f3bbaebb95fb9fa568992bdcdc810a5a6fd44
Simon Hanmer <simon.hanmer@globallogic.com>
2024-05-04 18:56:24 +0100

    update step 2 terraform files

---
+++ steps/step_2/s3.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "this" {
+  bucket = local.url
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.this.arn}/*"
+      }
+    ]
+  })
+  depends_on = [ aws_s3_bucket_public_access_block.this ]
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+  suffix = "index.html"
+  }
+
+  error_document {
+  key = "error.html"
+  }
+}
